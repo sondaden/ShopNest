5c029132eca9d11c259f292ff894d263303c04c8
Son <sonhbt.23it@vku.udn.vn>
2025-11-27 18:03:24 +0700

    feat: add custom methods findByName and findByPrice in ProductRepository

--- src/main/java/com/sonnhuynhh/shopnest/repository/ProductRepository.java
+++ src/main/java/com/sonnhuynhh/shopnest/repository/ProductRepository.java
@@ -3,6 +3,12 @@ package com.sonnhuynhh.shopnest.repository;
 import com.sonnhuynhh.shopnest.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product getProductsById(long id);
+//    Product getProductsById(long id);
+
+    List<Product> findByNameContainingIgnoreCase(String name);
+    List<Product> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
 }
